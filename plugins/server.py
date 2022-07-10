fa4dd98e895f73df68b22d8ebc9492ade7c4e4c0
SalahHemda <109025805+SalahHemda@users.noreply.github.com>
2022-07-10 15:53:59 +0200

    Add files via upload

---
+++ plugins/server.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from pyrogram.types import Message
+from modules.helpers.command import commandpro
+from pyrogram import Client, filters
+from os import system, execle, environ
+from modules.helpers.decorators import sudo_users_only
+from modules.config import BOT_USERNAME
+
+
+@Client.on_message(commandpro(["ريستارت", "اعادة", "/restart@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def restart_bot(_, message: Message):
+    msg = await message.reply("`ʀᴇsᴛᴀʀᴛɪɴɢ ʙᴏᴛ...`")
+    args = [sys.executable, "main.py"]
+    await msg.edit("⏳ جاري اعادة تشغيل البوت\n⌛ استناني دقيقه وشغلني متستعجلش")
+    execle(sys.executable, *args, environ)
+    return
